2b3547abdd43ee1a2458e9b0f6a38f56cba76fc7
shaka <jaherm9@gmail.com>
2021-11-04 19:54:23 +0600

    69-5 Finish Service and Explore Material UI Common Utility

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,17 +1,14 @@
 import { CardMedia, Grid } from '@mui/material';
 import React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 const Service = (props) => {
     const {name, description, img} = props.service;
     return (
         <Grid item xs={2} sm={4} md={4}>
-            <Card sx={{ minWidth: 275 }}>
+            <Card sx={{ minWidth: 275, border: 0, boxShadow: 0 }}>
             <CardMedia
         component="img"
         style={{width: 'auto', height: '80px', margin: '0 auto' }}
@@ -19,9 +16,6 @@ const Service = (props) => {
         alt="green iguana"
       />
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
         <Typography variant="h5" component="div">
           {name}
         </Typography>
@@ -34,9 +28,7 @@ const Service = (props) => {
          {description}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      
     </Card>
         </Grid>
     );
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
+import Typography from '@mui/material/Typography';
 import flouride from '../../../images/fluoride.png';
 import cavity from '../../../images/cavity.png';
 import whitening from '../../../images/whitening.png';
@@ -31,6 +32,12 @@ const Services = () => {
         
             <Box sx={{ flexGrow: 1 }}>
     <Container>
+    <Typography sx={{ fontWeight: 500, color: 'success.main',  m: 2 }} variant="h6" component="div">
+          OUR SERVICES
+        </Typography>
+    <Typography sx={{ fontWeight: 600, m: 5}} variant="h6" component="div">
+          SERVICES WE PROVIDE
+        </Typography>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {
             services.map(service => <Service
