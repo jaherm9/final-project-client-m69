3cdde2f155a66d066e59e8b790177f3726c33406
shaka <jaherm9@gmail.com>
2021-11-05 12:56:44 +0600

    70-3 Set Dynamic Calendar With Lift Up State To Share Data

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,14 +1,16 @@
+import { setDate } from 'date-fns';
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
 import AvailableAppointment from '../AvailableAppointment/AvailableAppointment';
 
 const Appointment = () => {
+    const [date, setDate] = React.useState(new Date());
     return (
         <div>
             <Navigation></Navigation>
-            <AppointmentHeader></AppointmentHeader>
-            <AvailableAppointment></AvailableAppointment>
+            <AppointmentHeader date={date} setDate={setDate}></AppointmentHeader>
+            <AvailableAppointment date={date}></AvailableAppointment>
         </div>
     );
 };
--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import { Container, Grid } from "@mui/material";
+import React from "react";
+import chair from '../../../images/chair.png'
+import Calendar from "../../Shared/Calendar/Calendar";
 
-const AppointmentHeader = () => {
-    return (
-        <div>
-            <h2>Appointment Header</h2>
-        </div>
-    );
+const AppointmentHeader = ({date, setDate}) => {
+    
+  return (
+    <Container>
+    <Grid Container spacing={2}>
+      <Grid item xs={12} md={6}>
+        <Calendar></Calendar>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <img style={{ width:'100%'}} src={chair} alt="" />
+      </Grid>
+    </Grid>
+    </Container>
+    
+  );
 };
 
 export default AppointmentHeader;
