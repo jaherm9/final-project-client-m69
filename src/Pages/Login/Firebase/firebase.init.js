1a73aa004ea9bd5b329b353e0878854066602f6a
shaka <jaherm9@gmail.com>
2021-11-06 23:25:00 +0600

    71-5 Set UseFirebase function and explain onAuthStateChanged

---
+++ src/Pages/Login/Firebase/firebase.init.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+const initializeFirebase = () => {
+  initializeApp(firebaseConfig);
+};
+
+export default initializeFirebase;
+
+/* 
+step for authentication
+--------------------
+Step-1: Initial Setup
+1. firebase: create project
+2. create web app
+3. get configuration
+4. initialize firebase
+5. Enable auth method
+---------------------
+Step-2: setup component
+1. Create Login Component
+2. Create Register Component
+3. Create Route for Login and Register
+----------------------
+Step-3: set auth system
+1. set up sign in method
+2. setup sign out method
+3. user state
+4. special observer
+5. return necessary method and states from useFirebase
+-----------------------
+Step-4: create auth context hook (useAuth)
+1. create a auth context
+2. create context Provider
+3. set context Provider context value
+
+*/
