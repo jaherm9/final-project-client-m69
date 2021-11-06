23ada0ae7083f2d71996e4a621f88b2960a44b54
shaka <jaherm9@gmail.com>
2021-11-06 10:37:34 +0600

    70-5 Style Booking Card Using Material Ui Paper

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,41 +1,41 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import Booking from "../Booking/Booking";
 
 const bookings = [
   {
     id: 1,
-    Name: "Teeth Orthodontics",
+    name: "Teeth Orthodontics",
     time: "8:00 AM - 9:00 AM",
     space: 10,
   },
   {
     id: 2,
-    Name: "Cosmetic Dentistry",
+    name: "Cosmetic Dentistry",
     time: "10:05 AM - 11:30 AM",
     space: 10,
   },
   {
     id: 3,
-    Name: "Teetch Cleaning",
+    name: "Teetch Cleaning",
     time: "5:00 PM - 6:30 PM",
     space: 10,
   },
   {
     id: 4,
-    Name: "Cabity Protection",
+    name: "Cabity Protection",
     time: "7:00 AM - 8:30 AM",
     space: 10,
   },
   {
     id: 5,
-    Name: "Teeth Orthodontics",
+    name: "Teeth Orthodontics",
     time: "8:00 AM - 9:00 AM",
     space: 10,
   },
   {
     id: 6,
-    Name: "Teeth Orthodontics",
+    name: "Teeth Orthodontics",
     time: "8:00 AM - 9:00 AM",
     space: 10,
   },
@@ -44,8 +44,7 @@ const bookings = [
 const AvailableAppointment = ({ date }) => {
   return (
     <Container>
-      <h2>Available Appointments: {date.toDateString()}</h2>
-
+      <Typography>Available Appointments: {date.toDateString()}</Typography>
       <Grid container spacing={2}>
         {bookings.map((booking) => (
           <Booking key={booking.id} booking={booking}></Booking>
--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -1,11 +1,30 @@
 import { Grid } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import React from "react";
 
-const Booking = ({booking}) => {
-    const {name, time, space} = bookings
+const Booking = ({ booking }) => {
+  const { name, time, space } = booking;
   return (
     <Grid item xs={12} sm={6} md={4}>
-      <h2>This is booking</h2>
+      <Paper elevation={3} sx={{ py: 5 }}>
+        <Typography
+          sx={{ color: "info.main", fontWeight: 600 }}
+          variant="h5"
+          gutterBottom
+          component="div"
+        >
+          {name}
+        </Typography>
+        <Typography variant="h6" gutterBottom component="div">
+          {time}
+        </Typography>
+        <Typography variant="caption" display="block" gutterBottom>
+          {space} SPACES AVAILABLE
+        </Typography>
+        <Button variant="contained">Book Appointment</Button>
+      </Paper>
     </Grid>
   );
 };
