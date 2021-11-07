4fb8abee314a2f0f4aad9a244a0fecccdda8098a
shaka <jaherm9@gmail.com>
2021-11-08 01:14:53 +0600

    71-9 Private Route and redirect user to the page they wanted to go

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import AuthProvider from "./Context/AuthProvider/AuthProvider";
 import Appointment from "./Pages/Appointment/Appointment/Appointment";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login/Login";
+import { PrivateRoute } from "./Pages/Login/PrivateRoute/PrivateRoute";
 import Register from "./Pages/Login/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/appointment" element={<Appointment />} />
-        <Route path="about" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <PrivateRoute path="/appointment" element={<Appointment />} />
+          <Route path="about" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
       </AuthProvider>
     </div>
   );
