68688869221d9ba5cb1ea151eba62e1b8c23d87b
shaka <jaherm9@gmail.com>
2021-11-08 10:08:50 +0600

    downgrade react-router-dom v5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AuthProvider from "./Context/AuthProvider/AuthProvider";
 import Appointment from "./Pages/Appointment/Appointment/Appointment";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login/Login";
-import { PrivateRoute } from "./Pages/Login/PrivateRoute/PrivateRoute";
+import PrivateRoute from "./Pages/Login/PrivateRoute/PrivateRoute";
+
 import Register from "./Pages/Login/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <PrivateRoute path="/appointment" element={<Appointment />} />
-          <Route path="about" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Routes>
+        <Router>
+          <Switch>
+            <PrivateRoute path="/appointment">
+              <Appointment />
+            </PrivateRoute>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </AuthProvider>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+/* import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
@@ -9,4 +29,4 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
   document.getElementById("root")
-);
+); */
